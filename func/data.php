3ab5a5826e1c953f907b8bf47f23b6b95c818fcc
frank-sng <frank.beehive@gmail.com>
2018-06-27 11:32:21 -0700

    updating

---
+++ func/data.php
@@ -0,0 +1,16 @@
+<?php
+
+// Data Type 
+$parcel_id = $_GET['p'];
+
+
+/* 
+
+    - Api's that we can call
+    * https://www.melissadata.com/ - Data Provider
+    * http://multcoproptax.org/ - County Lookup
+    * https://www.listsource.com/ - Data Provider
+    * https://makeloveland.com/ - Data Provider
+    * https://www.prospectnow.com/ - Data Provider
+
+*/
